bb99e0cc99c2ea5dd359c5aef716860d8be71a8f
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-07 12:41:29 +0300

    Fixes in Calculator

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -2,8 +2,9 @@
 public class Calculator {
     public static void main(String[] args) {
         int firstNum = 2;
-        int secondNum = 3;
+        int secondNum = 0;
         char operator = '^';
+
         if (operator == '+') {
             System.out.println (firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
         } else if (operator == '-') {
@@ -14,8 +15,12 @@ public class Calculator {
             System.out.println (firstNum + " / " + secondNum + " = " + (firstNum / secondNum));
         } else if (operator == '^') {
             int powResult = firstNum;
-            for (int i = 1; i < secondNum; i++) {
-                powResult = powResult * firstNum;
+            if (secondNum == 0) {
+                powResult = 1;
+            } else {
+                for (int i = 1; i < secondNum; i++) {
+                    powResult *= firstNum;
+                }
             }
             System.out.println (firstNum + " ^ " + secondNum + " = " + powResult);
         } else if (operator == '%') {
