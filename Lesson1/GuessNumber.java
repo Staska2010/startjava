4533e41be076e60a0613d2e5064672a4f3840e58
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-07 20:00:49 +0300

    GuessNumber

--- Lesson1/GuessNumber.java
+++ Lesson1/GuessNumber.java
@@ -1,63 +1,28 @@
 public class GuessNumber {
 	public static void main(String[] args) {
-		int computer = 10;
-		int user1 = 4;
-		int user2 = 12;
-		int user3 = 10;
-		int user4 = 7;
-		int user5 = 1;
-		boolean result = false;
+		// computer - значение, которое загадал компьютер
+		int computer = 74;
+		//Переменные для двоичного поиска
+		// left - левая и правые границы интервала [0; 100]
+		// qurrent - текущее ("вводимое" пользователем) значение
+		int left = 0;
+		int right = 100;
+		int qurrent = 0;
 
-		do {
-			if (user1 > computer) {
-				System.out.println("Your number is greater !");
-			} else if (user1 < computer) {
-				System.out.println("Your number is less !");
-			} else {
-				result = true;
-				System.out.println("You guessed !");
+		// Сам двоичный поиск
+		while (left <= right) {
+			qurrent = (left + right) / 2;
+			System.out.println ("Your choice:" + qurrent);
+			if (qurrent == computer) {
+				System.out.println ("You won!");
 				break;
-			}
-
-			if (user2 > computer) {
-				System.out.println("Your number is greater !");
-			} else if (user2 < computer) {
-				System.out.println("Your number is less !");
+			} else if (qurrent > computer) {
+				right = qurrent;
+				System.out.println("You number is greater!");
 			} else {
-				result = true;
-				System.out.println("You guessed !");
-				break;
+				left = qurrent;
+				System.out.println("Your number is less!");
 			}
-
-			if (user3 > computer) {
-				System.out.println("Your number is greater !");
-			} else if (user3 < computer) {
-				System.out.println("Your number is less !");
-			} else {
-				result = true;
-				System.out.println("You guessed !");
-				break;
-			}
-
-			if (user4 > computer) {
-				System.out.println("Your number is greater !");
-			} else if (user4 < computer) {
-				System.out.println("Your number is less !");
-			} else {
-				result = true;
-				System.out.println("You guessed !");
-				break;
-			}
-
-			if (user5 > computer) {
-				System.out.println("Your number is greater !");
-			} else if (user5 < computer) {
-				System.out.println("Your number is less !");
-			} else {
-				result = true;
-				System.out.println("You guessed !");
-				break;
-			}
-		} while (result != true);
+		}
 	}
 }
