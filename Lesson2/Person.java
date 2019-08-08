f0d2240087ec2faa8a3f459e710ffc4f2b19ebf3
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-08 16:27:27 +0300

    Peron/fixes

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -6,26 +6,20 @@ public class Person {
 	String name = "John Smith";
 	String hairColor = "dark";
 
-	public Person(int height, int weight, int age, boolean isMan, String name, String hairColor) {
-		this.height = height;
-		this.weight = weight;
-		this.age = age;
-		this.isMan = isMan;
-		this.name = name;
-		this.hairColor = hairColor; 
+	public void walk(int meters) {
+
 	}
+
+	public void sit() {
+
+	} 
 
 	public void run(int meters) {
 
 	}
 
 	public void say(String somethingToSay) {
-		System.out.println(somethingToSay);
-
-	}
-
-	public void sleep(int howManyTime) {
-
+		
 	}
 
 	public void learnJava(int howManyHours) {
