c997f4e14324cf406274defd5ac0c75f6d6556e8
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-09 19:06:32 +0300

    Fixes in Calculator with classes

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -2,23 +2,30 @@
 public class Calculator {
     public static void run(int firstNum, char operator, int secondNum) {   
         switch (operator) {
-            case '+' : System.out.println (firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
-                        break;
-            case '-' : System.out.println (firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
-                        break;
-            case '*' : System.out.println (firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
-                        break;
-            case '/' : System.out.println (firstNum + " / " + secondNum + " = " + (firstNum / secondNum));
-                        break;
-            case '^' : int powResult = 1;
-                        for (int i = 1; i <= secondNum; i++) {
-                            powResult *= firstNum;
-                        }
-                        System.out.println(firstNum + " ^ " + secondNum + " = " + powResult);
-                        break;
-            case '%' : System.out.println (firstNum + " % " + secondNum + " = " + (firstNum % secondNum));
-                        break;
-            default : System.out.println("Wrong operator");
+            case '+' : 
+                System.out.println (firstNum + " + " + secondNum + " = " + (firstNum + secondNum));
+                break;
+            case '-' : 
+                System.out.println (firstNum + " - " + secondNum + " = " + (firstNum - secondNum));
+                break;
+            case '*' : 
+                System.out.println (firstNum + " * " + secondNum + " = " + (firstNum * secondNum));
+                break;
+            case '/' : 
+                System.out.println (firstNum + " / " + secondNum + " = " + (firstNum / secondNum));
+                break;
+            case '^' : 
+                int powResult = 1;
+                for (int i = 1; i <= secondNum; i++) {
+                    powResult *= firstNum;
+                }
+                System.out.println(firstNum + " ^ " + secondNum + " = " + powResult);
+                break;
+            case '%' : 
+                System.out.println (firstNum + " % " + secondNum + " = " + (firstNum % secondNum));
+                break;
+            default : 
+                System.out.println("Wrong operator");
         }
     }
 }
