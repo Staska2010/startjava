0a84135bd0f11285ca440ee2c935235211075cd0
Stanislav Klokov <klokovstas2013@gmail.com>
2019-08-16 22:44:23 +0300

    Fixes in Calculator

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -1,27 +1,30 @@
 package com.startjava.lesson_4.calculator;
 
-public class Calculator {
-    public static void calculate(int firstNum, char operator, int secondNum) {
+class Calculator {
+     void calculate(String[] mathExpression) {
+        int firstNum = Integer.parseInt(mathExpression[0]);
+        char operator = mathExpression[1].charAt(0);
+        int secondNum = Integer.parseInt(mathExpression[2]);
         switch (operator) {
-            case '+' : 
+            case '+' :
                 System.out.println (firstNum + " + " + secondNum + " = " + Math.addExact(firstNum, secondNum));
                 break;
-            case '-' : 
+            case '-' :
                 System.out.println (firstNum + " - " + secondNum + " = " + Math.subtractExact(firstNum, secondNum));
                 break;
-            case '*' : 
+            case '*' :
                 System.out.println (firstNum + " * " + secondNum + " = " + Math.multiplyExact(firstNum, secondNum));
                 break;
-            case '/' : 
+            case '/' :
                 System.out.println (firstNum + " / " + secondNum + " = " + Math.floorDiv(firstNum, secondNum));
                 break;
             case '^' :
                 System.out.println(firstNum + " ^ " + secondNum + " = " + Math.pow(firstNum, secondNum));
                 break;
-            case '%' : 
+            case '%' :
                 System.out.println (firstNum + " % " + secondNum + " = " + Math.floorMod(firstNum, secondNum));
                 break;
-            default : 
+            default :
                 System.out.println("Wrong operator");
         }
     }
--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.startjava.lesson_4.calculator;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
@@ -10,12 +9,13 @@ public class CalculatorTest {
 			System.out.println("Enter the expression: ");
 			Scanner sc = new Scanner(System.in);
 			String input = sc.nextLine();
-				String[] splitInput = input.split("\\s");
-				Calculator.calculate(Integer.parseInt(splitInput[0]), splitInput[1].charAt(0), Integer.parseInt(splitInput[2]));
-				do {
-					System.out.println("Do you want to continue? (yes/no): ");
-					confirmation = sc.nextLine();
-				} while (!confirmation.equals("yes") && !confirmation.equals("no"));
+			String[] mathExpression = input.split("\\s");
+			Calculator calculator = new Calculator();
+			calculator.calculate(mathExpression);
+			do {
+				System.out.println("Do you want to continue? (yes/no): ");
+				confirmation = sc.nextLine();
+			} while (!confirmation.equals("yes") && !confirmation.equals("no"));
 		} while(confirmation.equals("yes"));
 	}
 }
